bbc92b2fea7e7f4bd8df6a60d8efcb988a914398
Petr Spiroch <283240@vutbr.cz>
2026-04-20 13:08:45 +0200

    student_grades_final

---
+++ student_grades.py
@@ -0,0 +1,67 @@
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
+
+    def get_grade(self, index):
+        score = self.get_by_index(index)
+
+        if score >= 90:
+            return "A"
+        elif score >= 80:
+            return "B"
+        elif score >= 70:
+            return "C"
+        elif score >= 60:
+            return "D"
+        elif score >= 50:
+            return "E"
+        else:
+            return "F"
+
+    def find(self, value):
+        result = []
+
+        for i in range(len(self.scores)):
+            if self.scores[i] == value:
+                result.append(i)
+
+        return result
+
+    def get_sorted(self):
+        scores = self.scores.copy()
+        n = len(scores)
+
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if scores[j] > scores[j + 1]:
+                    scores[j], scores[j + 1] = scores[j + 1], scores[j]
+
+        return scores
+
+
+def main():
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+    print(results.count())
+    print(results.get_by_index(2))
+
+    print(results.get_grade(2))
+    print(results.get_grade(6))
+    print(results.get_grade(7))
+
+    print(results.find(100))
+    print(results.find(50))
+    print(results.find(77))
+
+    print(results.get_sorted())
+    print(results.scores)
+
+
+if __name__ == "__main__":
+    main()
