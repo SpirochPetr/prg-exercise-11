83415ff74cf62de6418a7b050972ad5537cb7ec7
Petr Spiroch <283240@vutbr.cz>
2026-04-20 12:52:26 +0200

    bubble_graf

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 import random
+import matplotlib.pyplot as plt
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -20,16 +21,39 @@ def selection_sort(numbers):
 
 #bubble############################################
 
-def bubble_sort(numbers):
-    numbers = numbers.copy()
-    n = len(numbers)
+def bubble_sort(values):
+    values = values.copy()
+    n = len(values)
+    plt.ion()
+    plt.show()
 
     for i in range(n):
-        for j in range(0, n - i - 1):
-            if numbers[j] > numbers[j + 1]:
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+        swapped = False
 
-    return numbers
+        for j in range(0, n - i - 1):
+            index_highlight1 = j
+            index_highlight2 = j + 1
+
+            colors = ["steelblue"] * len(values)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+
+            plt.clf()
+            plt.bar(range(len(values)), values, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+
+            if values[j] > values[j + 1]:
+                values[j], values[j + 1] = values[j + 1], values[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    plt.ioff()
+    plt.show()
+
+    return values
 
 #main########################################
 
@@ -45,12 +69,14 @@ def main():
     random_list = random_numbers(20)
     print("\nNáhodný seznam:", random_list)
     print("Seřazený seznam:", bubble_sort(random_list))
-
+    ##t3#
+    test = random_numbers(10)
+    print("Původní:", test)
+    sorted_list = bubble_sort(test)
+    print("Seřazený:", sorted_list)
 
 if __name__ == "__main__":
     main()
 
 
 
-if __name__ == "__main__":
-    main()
